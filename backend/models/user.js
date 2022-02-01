ff1493a81dc8d6b20b60e48d3b053bf608edbfc0
bishalpandit <bishalpandit17@gmail.com>
2022-02-01 12:26:50 +0530

    Added validatin for username

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,27 +8,49 @@ const validateMobile = (mob) => {
     for (let i = 0; i < mob.length; i++) {
         const asciiCode = mob.charCodeAt(i);
         if (asciiCode < 48 || asciiCode > 57)
-        return false;
+            return false;
     }
-    
-    if(mob.length !== 10)
+
+    if (mob.length !== 10)
         return false;
 
     return true;
 }
 
+const validateUsername = (username) => {
+
+
+    for (let i = 0; i < username.length; i++) {
+
+        let ch = username.charCodeAt(i)
+
+        if (ch == 32) // Space
+            return false;
+
+        // Check for alphanumeric values
+        if ((ch >= 48 && ch <= 57) || (ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122)) {
+            continue;
+        }
+
+        return false;
+    }
+
+    return true;
+}
+
+// alpha numeric || no spaces
 const userSchema = new Schema({
     username: {
         type: String,
         required: true,
         unique: true,
-        validate: [validator.isAlphanumeric, 'Enter only alhphanumeric values with no spaces']
+        validate: [(val) => validateUsername(val), 'Enter only alhphanumeric values with no spaces']
     }, // No spaces and only alphanumeric chars
     mobile: {
         type: String,
         required: true,
         unique: true,
-        validate: [(val) => validateMobile(val) , 'Please enter a valid mobile phone']
+        validate: [(val) => validateMobile(val), 'Please enter a valid mobile phone']
 
     }, // Only 10 digit
     email: {
